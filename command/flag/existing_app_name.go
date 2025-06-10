5cb1afadfd1063b027b8e2580be528664f8a861f
Marc Khouzam <marc.khouzam@broadcom.com>
2025-06-10 12:32:30 -0400

    Allow to extend completion for app name
    Signed-off-by: Marc Khouzam <marc.khouzam@broadcom.com>

--- command/flag/existing_app_name.go
+++ command/flag/existing_app_name.go
@@ -1,3 +1,13 @@
 package flag
 
+import (
+	flags "github.com/jessevdk/go-flags"
+)
+
 type ExistingAppName string
+
+var ExistingAppNameCompleteFunc = func(prefix string) []flags.Completion { return nil }
+
+func (ExistingAppName) Complete(prefix string) []flags.Completion {
+	return ExistingAppNameCompleteFunc(prefix)
+}
