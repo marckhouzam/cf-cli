adf3aca024267724bef0556644041210473ecbba
Marc Khouzam <marc.khouzam@broadcom.com>
2025-07-05 22:27:43 -0700

    Make AppType completion extensible
    With this approach is becomes possible to modify the completion logic
    for AppType by setting the new `AppTypeCompleteFunc` variable to a new
    function.
    
    Signed-off-by: Marc Khouzam <marc.khouzam@broadcom.com>

--- command/flag/app_type.go
+++ command/flag/app_type.go
@@ -4,6 +4,10 @@ import flags "github.com/jessevdk/go-flags"
 
 type AppType string
 
-func (AppType) Complete(prefix string) []flags.Completion {
+var AppTypeCompleteFunc = func(prefix string) []flags.Completion {
 	return completions([]string{"buildpack", "docker"}, prefix, false)
+}
+
+func (AppType) Complete(prefix string) []flags.Completion {
+	return AppTypeCompleteFunc(prefix)
 }
